f58251eda0852c9dc1a4ae67a0174db83c753243
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-09-09 02:07:39 +0100

    building out the template of the site

--- scripts/custom/smooth-scroll.js
+++ scripts/custom/smooth-scroll.js
@@ -9,7 +9,7 @@
         $target = $target.length && $target
         || $('[name=' + this.hash.slice(1) +']');
         if ($target.length) {
-          var targetOffset = $target.offset().top;
+          var targetOffset = $target.offset().top - 10;
           $('html,body')
             .animate({scrollTop: targetOffset}, 1000);
           return false;
